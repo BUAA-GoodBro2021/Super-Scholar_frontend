cba29b093ef2f250d9f44ce62578ce563d1990a7
李昊 <1097261154@qq.com>
2022-11-26 01:54:53 +0800

    feat: 添加登录界面

--- src/language/modules/en.js
+++ src/language/modules/en.js
@@ -2,6 +2,7 @@ export default {
     header:{
         fullScreen:"Full Screen",
         exitFullScreen:"Exit Full Screen",
-        action1:"Action 1",
+        action1:"logout",
+        welcome:"Welcome",
     }
 }
--- src/language/modules/zh.js
+++ src/language/modules/zh.js
@@ -2,6 +2,7 @@ export default {
     header:{
         fullScreen:"全屏",
         exitFullScreen:"取消全屏",
-        action1:"行为1",
+        action1:"退出登录",
+        welcome:"欢迎登录",
     }
 }
