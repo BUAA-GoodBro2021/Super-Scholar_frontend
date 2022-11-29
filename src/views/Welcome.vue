514135dc698820beb175fbae90f77d7eabd37a4a
Cloud-Iris <1661695051@qq.com>
2022-11-29 13:35:43 +0800

    fix: 对于页面布局header+main做出调整
    顺便修理了下welcome页面

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,27 @@
 <template>
-   <div class="welcome">
-      <Header/>
-      这里是主页
+   <div class="clearfix" style="height: 100vh;">
+      <Header style="height: 64px;"/>
+      <div class="client-body">
+         这里是主页
+      </div>
    </div>
 </template>
 <script setup>
 import Header from "../components/Header/index.vue"
 </script>
-<style>
-.welcome{
-   height: 100%;
+<style scoped>
+
+.clearfix::before,
+.clearfix::after{
+   content: '';
+   display: table;
+   clear: both;
+}
+
+.client-body {
+  margin-top: 64px;
+  height: calc(100vh - 64px);
+  border: 1px solid black;
+  box-sizing: border-box;
 }
 </style>
--- src/views/client/Index.vue
+++ src/views/client/Index.vue
@@ -1,9 +1,33 @@
 <template>
-   <Header />
-   <router-view></router-view>
+  <div class="client-main clearfix">
+    <Header class="client-header" />
+    <div class="client-body">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 <script setup>
 import Header from "../../components/Header/index.vue"
 </script>
 <style>
+.client-main {
+  height: 100vh;
+}
+/* 清除外边距折叠和高度塌陷 */
+.clearfix::before,
+.clearfix::after {
+  content: '';
+  display: table;
+  clear: both;
+}
+
+.client-header {
+  height: 64px;
+  /* 4rem */
+}
+
+.client-body {
+  margin-top: 64px;
+  height: calc(100vh - 64px);
+}
 </style>
