6726c07758495263880a36c6d3c5b50fbba805a4
hjc-owo <2535199657@qq.com>
2022-12-02 12:10:34 +0800

    add: 添加生成BiBTeX方法

--- src/router/index.js
+++ src/router/index.js
@@ -78,6 +78,11 @@ const router = createRouter({
           path: '/message-list',
           name: 'MessageList',
           component: ()=>import('../views/message/index.vue'),
+        },
+        {
+          path: '/institution/:institutionid',
+          name: 'InstitutionDetail',
+          component: ()=>import('../views/institution/InstitutionDetail.vue'),
         }
       ],
     },
